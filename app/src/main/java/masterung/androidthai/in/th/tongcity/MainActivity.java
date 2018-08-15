712564083239204e60c42e372545fb203e986617
masterUNG <phrombutr@gmail.com>
2018-08-15 13:20:11 +0700

    Initial commit

---
+++ app/src/main/java/masterung/androidthai/in/th/tongcity/MainActivity.java
@@ -0,0 +1,25 @@
+package masterung.androidthai.in.th.tongcity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import masterung.androidthai.in.th.tongcity.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment())
+                    .commit();
+        }
+
+
+    }   // Main  Method
+
+}   // Main Class
