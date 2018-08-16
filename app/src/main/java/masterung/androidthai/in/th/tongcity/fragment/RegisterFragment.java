f8ed0024c54ef67023893759bb523c13b42b714d
masterUNG <phrombutr@gmail.com>
2018-08-16 13:23:35 +0700

    Check TextField OK

--- app/src/main/java/masterung/androidthai/in/th/tongcity/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/tongcity/fragment/RegisterFragment.java
@@ -1,18 +1,35 @@
 package masterung.androidthai.in.th.tongcity.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import masterung.androidthai.in.th.tongcity.MainActivity;
 import masterung.androidthai.in.th.tongcity.R;
+import masterung.androidthai.in.th.tongcity.utility.MyAlert;
 
 public class RegisterFragment extends Fragment{
+
+    //    Explicit
+    private Uri uri;
+    private boolean aBoolean = true;
+    private String nameString, emailString, passwordString;
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -21,14 +38,122 @@ public class RegisterFragment extends Fragment{
 //        Create Toolbar
         createToolbar();
 
+//        Avata Controller
+        avataController();
 
     }   // Method
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            aBoolean = false;
+            uri = data.getData();
+            showAvata();
+
+        } else {
+            Toast.makeText(getActivity(),"Please Choose Image", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    private void showAvata() {
+
+        try {
+
+            Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+            ImageView imageView = getView().findViewById(R.id.imvAvata);
+
+            Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap, 800, 480, true);
+            imageView.setImageBitmap(bitmap1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void avataController() {
+
+        ImageView imageView = getView().findViewById(R.id.imvAvata);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Choose Image"), 1);
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemUpload) {
+            uploadAvata();
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void uploadAvata() {
+
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText emailEditText = getView().findViewById(R.id.edtEmail);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+        nameString = nameEditText.getText().toString().trim();
+        emailString = emailEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+
+        if (aBoolean) {
+            MyAlert myAlert = new MyAlert(getActivity());
+            myAlert.normalDialog("Non Image", "Please Choose Image");
+        } else if (nameString.isEmpty() || emailString.isEmpty() || passwordString.isEmpty()) {
+            MyAlert myAlert = new MyAlert(getActivity());
+            myAlert.normalDialog("Have Space", "Please Fill All Blank");
+        } else {
+            registerFirebase();
+        }
+
+
+    }
+
+    private void registerFirebase() {
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_register, menu);
+
+    }
 
     private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
         ((MainActivity) getActivity()).setSupportActionBar(toolbar);
-        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(R.string.register);
         ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please Fill in Blank");
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+        setHasOptionsMenu(true);
 
     }
 
